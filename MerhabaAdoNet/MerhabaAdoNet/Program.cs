2eeaed701e08e164c7b6b89706db147d2620810f
sametkapar <kaparsamet@gmail.com>
2024-09-15 09:56:05 +0300

    AdoNet

---
+++ MerhabaAdoNet/MerhabaAdoNet/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MerhabaAdoNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Ado İle Veri Çekme - Kategori Sayısını Çekelim
+
+            ////1 - Bağlamtı
+            //SqlConnection baglanti = new SqlConnection();
+
+            ////2 - Bağlntı Yolu 
+            //baglanti.ConnectionString = "Data Source=DESKTOP-IRAMQ7M\\SQLEXPRESS;Initial Catalog=NORTHWND;Integrated Security=True";
+
+            ////3- Komut
+            //SqlCommand komut = new SqlCommand();
+            //komut.Connection = baglanti;
+
+            ////4- Komut Metni
+            //komut.CommandText = "SELECT COUNT(*) FROM Categories";
+
+            ////5- Bağlantı Aç
+            //baglanti.Open();
+
+            ////6- Sorgu Çalıştır
+            //int sayi = Convert.ToInt32(komut.ExecuteScalar());
+
+            ////7-Baglanti Kapat
+            //baglanti.Close();
+
+
+            //Console.WriteLine("Kategori Sayısı = " + sayi);
+
+            #endregion
+
+            #region Üstteki sorguyu azcık kısaltalım
+
+            //SqlConnection baglanti = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=NORTHWND; Integrated Security=True");
+
+            //SqlCommand komut = baglanti.CreateCommand();
+
+            //komut.CommandText = "SELECT COUNT(*) FROM Products";
+
+            //baglanti.Open();
+
+            //int sayi = Convert.ToInt32(komut.ExecuteScalar());
+
+            //Console.WriteLine("Ürün Sayısı = " + sayi);
+
+            //baglanti.Close();
+            #endregion
+
+            #region Kategori Ekleyelim
+
+            //Console.WriteLine("NorthWind LTD'ye hoşgeldiniz");
+            //Console.Write("Kategori Adi = ");
+            //string isim = Console.ReadLine();
+            //Console.Write("Açıklama = ");
+            //string aciklama = Console.ReadLine();
+
+            //SqlConnection baglanti = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=NORTHWND; Integrated Security=True");
+
+            //SqlCommand komut = baglanti.CreateCommand();
+
+            //komut.CommandText = "INSERT INTO Categories(CategoryName, Description) VALUES('" + isim + "','" + aciklama + "')";
+            //baglanti.Open();
+            //komut.ExecuteNonQuery();
+            //baglanti.Close();
+            #endregion
+
+            #region Kategorileri Listeyelim
+
+            //SqlConnection baglanti = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=NORTHWND; Integrated Security=True");
+            //SqlCommand komut = baglanti.CreateCommand();
+
+            //komut.CommandText = "SELECT Description, CategoryID, CategoryName FROM Categories";
+            ////Sıralama önemli 0 ve 1 leri kontrol et
+            //baglanti.Open();
+            //SqlDataReader okuyucu = komut.ExecuteReader();
+            //while (okuyucu.Read()) //Okunmamış satır var ise true döndürür
+            //{
+
+            //    int id = okuyucu.GetInt32(1);
+            //    string isim = okuyucu.GetString(2);
+            //    string aciklama = "";
+            //    if (!okuyucu.IsDBNull(0))
+            //    {
+            //        aciklama = okuyucu.GetString(0);
+            //    }
+            //    else
+            //    {
+            //        aciklama = "Görüyorsunuz açıklamaya gerek yok!!!";
+            //    }
+            //    Console.WriteLine($"{id}) {isim} - {aciklama}");
+
+            //}
+            //baglanti.Close();
+
+
+            #endregion
+
+            #region Kategorideki ürünler
+
+            SqlConnection baglanti = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=NORTHWND; Integrated Security=True");
+            SqlCommand komut = baglanti.CreateCommand();
+
+            komut.CommandText = "SELECT CategoryID, CategoryName, Description FROM Categories";
+            //Sıralama önemli 0 ve 1 leri kontrol et
+            baglanti.Open();
+            SqlDataReader okuyucu = komut.ExecuteReader();
+            while (okuyucu.Read()) //Okunmamış satır var ise true döndürür
+            {
+
+                int id = okuyucu.GetInt32(0);
+                string isim = okuyucu.GetString(1);
+                string aciklama = "";
+                if (!okuyucu.IsDBNull(2))
+                {
+                    aciklama = okuyucu.GetString(2);
+                }
+                else
+                {
+                    aciklama = "Görüyorsunuz açıklamaya gerek yok!!!";
+                }
+                Console.WriteLine($"{id}) {isim} - {aciklama}");
+
+            }
+            baglanti.Close();
+
+
+            Console.WriteLine("Lütfen ürünleri listelemek istedğiniz kategorinin numarasını giriniz");
+            string katNo = Console.ReadLine();
+            SqlConnection baglanti2 = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=NORTHWND; Integrated Security=True");
+            SqlCommand komut2 = baglanti2.CreateCommand();
+
+            komut2.CommandText = "SELECT ProductID, ProductName, UnitPrice, UnitsInStock FROM Products WHERE CategoryID= " + katNo;
+            baglanti2.Open();
+            SqlDataReader okuyucu2 = komut2.ExecuteReader();
+
+            while (okuyucu2.Read()) 
+            {
+                int id = okuyucu2.GetInt32(0);
+                string isim = okuyucu2.GetString(1);
+                decimal fiyat = okuyucu2.GetDecimal(2);
+                short stok = okuyucu2.GetInt16(3);
+                Console.WriteLine($"{id}) {isim} {stok} {fiyat} TL");
+
+            }
+
+            baglanti2.Close();
+
+            #endregion
+        }
+    }
+}
---
+++ MerhabaAdoNet/MerhabaAdoNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MerhabaAdoNet")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MerhabaAdoNet")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d9967e71-0ca9-42be-982d-90d3dd29ccec")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
